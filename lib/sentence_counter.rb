c95b2b04ea3c4747f4e510333721d17b3cae2b5f
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-07 14:20:16 -0600

    Finishes count method, returns hash with count of sentences

--- lib/sentence_counter.rb
+++ lib/sentence_counter.rb
@@ -6,7 +6,11 @@ class SentenceCounter
   end
 
   def count(file)
+    hash = Hash.new(0)
     @sentences = format(file)
+    @sentences.each do |sentence|
+      hash[sentence] += 1
+    end
   end
 
   def format(file)
